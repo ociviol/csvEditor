2cc4576da5c86dab7aeaf1957756b631ad8790aa
Ollivier Civiol <ollivier@civiol.eu>
2020-02-25 21:30:49 +0100

    Making new project

---
+++ csvEditor/main.pas
@@ -0,0 +1,31 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    StringGrid1: TStringGrid;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
